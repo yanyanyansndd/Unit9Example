972844da77c85ddca72f58ab9d325b82439a4798
yanyanyansndd <396987602@qq.com>
2021-11-19 15:59:08 +0800

    Unit9Example

---
+++ src/TotalArea.java
@@ -0,0 +1,53 @@
+public class TotalArea {
+    /** Main method */
+	public static void main(String[] args) {
+        //Declare circleArray
+		CircleWithPrivateDataFields[] circleArray;
+		
+		//Create circleArray
+		circleArray = createCircleArray();
+		
+		//Print circleArray and total areas of the circles
+		printCircleArray(circleArray);
+	}
+	
+	/** Create an array of CircleWithPrivateDataFields objects */
+	public static CircleWithPrivateDataFields[] createCircleArray() {
+		CircleWithPrivateDataFields[] circleArray = new CircleWithPrivateDataFields[5];
+		
+		for(int i = 0; i < circleArray.length; i++) {
+			circleArray[i] = new CircleWithPrivateDataFields(Math.random() * 100);
+		}
+		
+		//Return CircleWithPrivateDataFields Array
+		return circleArray;
+	}
+	
+	/** Print an array of circles and their total area */
+	public static void printCircleArray(CircleWithPrivateDataFields[] circleArray) {
+		System.out.printf("%-30s%-15s\n", "Radius", "Area");
+		for(int i = 0; i < circleArray.length; i++) {
+			System.out.printf("%-30f%-15f\n", circleArray[i].getRadius(),
+					circleArray[i].getArea());
+		}
+		
+		System.out.println("______________________________________________");
+		
+		// Compute and display the result
+		System.out.printf("%-30s%-15f\n", "The total area of circles is",
+				sum(circleArray));
+	}
+	
+	/** Add circle areas */
+	public static double sum(CircleWithPrivateDataFields[] circleArray) {
+		// Initialize sum
+		double sum = 0;
+		
+		//Add areas to sum
+		for(int i = 0; i < circleArray.length; i++)
+			sum += circleArray[i].getArea();
+		
+		return sum;
+	}
+
+}
